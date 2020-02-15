a0fc218176c8eb2e8ee2db1ff72ca867618135ab
Tóth Kristóf <tkris@vipmail.hu>
2020-02-15 11:01:05 +0100

    refactor error handling

--- Player.js
+++ Player.js
@@ -4,11 +4,16 @@ class Player {
   }
 
   static betRequest(gameState, bet) {
+
     const currentPlayer = gameState.players[gameState.in_action];
     const holeCards = currentPlayer.hole_cards;
+    if(holeCards.length !== 2){
+      bet(0);
+      return;
+    }
+
     const isPair = holeCards[0].rank === holeCards[1].rank;
     if (
-      holeCards.length === 2 &&
       rank2numeric(holeCards[0].rank) >= 10 && isPair) {
       bet(gameState.current_buy_in - currentPlayer.bet + gameState.minimum_raise);
     } else {
