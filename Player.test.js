e7ac27b95a607d38dd9020dc4a5a16878ef56a03
Simon Kornél <simon.kornel@mikrum.hu>
2020-02-15 10:48:16 +0100

    basic test

--- Player.test.js
+++ Player.test.js
@@ -1,3 +1,29 @@
+const Player = require('./Player');
+
 test('adds 1 + 2 to equal 3', () => {
   expect(3).toBe(3);
+});
+
+test('default', (done) => {
+  Player.betRequest({
+    in_action: 0,
+    current_buy_in: 10,
+    minimum_raise: 1,
+    players: [
+      {
+        bet: 2,
+        hole_cards: [
+          {
+            rank: "10"
+          },
+          {
+            rank: "10"
+          }
+        ]
+      }
+    ]
+  }, value => {
+    expect(value).toBe(9);
+    done();
+  });
 });
