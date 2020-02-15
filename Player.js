23513b9ef420e8619703360680e69360e5e6a066
Kálmán Tarnay <kalman@tarnay.me>
2020-02-15 10:49:43 +0100

    Add rank2numberic convenience function

--- Player.js
+++ Player.js
@@ -7,8 +7,9 @@ class Player {
     const currentPlayer = gameState.players[gameState.in_action];
     const holeCards = currentPlayer.hole_cards;
     const isPair = holeCards[0].rank === holeCards[1].rank;
-    const goodRanks = ["10", "J", "Q", "K", "A"];
-    if (holeCards.length === 2 && goodRanks.indexOf(holeCards[0].rank) !== -1 && isPair) {
+    if (
+      holeCards.length === 2 &&
+      rank2numeric(holeCards[0].rank) >= 10 && isPair) {
       bet(gameState.current_buy_in - currentPlayer.bet + gameState.minimum_raise);
     } else {
       bet(0);
@@ -16,6 +17,21 @@ class Player {
   }
 
   static showdown(gameState) {
+  }
+}
+
+function rank2numeric(rank) {
+  switch(rank) {
+    case "A":
+      return 14;
+    case "K":
+      return 13;
+    case "Q":
+      return 12;
+    case "J":
+      return 11;
+    default:
+      return Number(rank);
   }
 }
 
