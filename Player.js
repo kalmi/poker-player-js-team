5de56d3196c98db4c7291dcd5a80f54612aa20ff
Kálmán Tarnay <kalman@tarnay.me>
2020-02-15 10:25:52 +0100

    Quick start

--- Player.js
+++ Player.js
@@ -4,7 +4,15 @@ class Player {
   }
 
   static betRequest(gameState, bet) {
-    bet(0);
+    const currentPlayer = gameState.players[gameState.in_action];
+    const holeCards = currentPlayer.hole_cards;
+    const isPair = holeCards[0].rank === holeCards[1].rank;
+    const goodRanks = ["10", "J", "Q", "K", "A"];
+    if (goodRanks.indexOf(holeCards[0].rank) !== -1 && isPair) {
+      bet(gameState.current_buy_in - currentPlayer.bet + gameState.minimum_raise);
+    } else {
+      bet(0);
+    }
   }
 
   static showdown(gameState) {
